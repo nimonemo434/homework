a7036985c861365455356ab0b4e816495049df44
nimonemo434 <109446732+nimonemo434@users.noreply.github.com>
2022-07-20 09:30:41 +0900

    homework
    homework

---
+++ homework kimdongrok/homework kimdongrok/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace homework_kimdongrok
+{
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            System.Console.Write("Hello world");
+            System.Console.ReadKey();
+        }
+    }
+}
