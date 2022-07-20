a87de291eba23dc3b08fc4eb33442793a76b046d
nimonemo434 <109446732+nimonemo434@users.noreply.github.com>
2022-07-20 11:46:45 +0900

    homework
    homework

--- homework kimdongrok/homework kimdongrok/Program.cs
+++ homework kimdongrok/homework kimdongrok/Program.cs
@@ -1,11 +1,34 @@
-﻿namespace homework_kimdongrok
+﻿using System;
+
+namespace homework_kimdongrok
 {
     public class HelloWorld
     {
         public static void Main()
         {
-            System.Console.Write("Hello world");
-            System.Console.ReadKey();
+            // 1
+            {
+                Console.Write("Hello world");
+                Console.ReadLine();
+            }
+
+            // 2
+            {
+                string s1;
+                string s2;
+                string s3;
+
+                Console.Write("input s1 : ");
+                s1 = Console.ReadLine();
+                System.Console.Write("input s2 : ");
+                s2 = Console.ReadLine();
+
+                Console.WriteLine($"the string is \n{s1}, {s2}");
+                Console.WriteLine("the string is \n{0}, {1}", s1, s2);
+                Console.WriteLine("the string is \n{1}, {0}", s1, s2);
+                Console.WriteLine("the string is" + "\n" + "{0}" + ", " + "{1}", s1, s2);
+                Console.ReadKey();
+            }
         }
     }
 }
