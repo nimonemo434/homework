0e51877b1e0c805cdc6cd75eaf7d61b653f9de13
nimonemo434 <109446732+nimonemo434@users.noreply.github.com>
2022-07-21 09:54:28 +0900

    03
    03

---
+++ homework 03/homework 03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+            s1 = Console.ReadLine();
+
+            int i1;
+            i1 = int.Parse(s1);
+            Console.WriteLine(i1);
+
+            float f1;
+            f1 = (float)i1;
+            Console.WriteLine(f1);
+
+            s1 = Convert.ToString(f1);
+            Console.WriteLine(s1);
+
+            Console.ReadKey();
+        }
+    }
+}
